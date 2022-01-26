1bed925c3eb4c82afd1c76ff2ecbaa5bfd9a6079
Atif Riaz <a.riaz@cranfield.ac.uk>
2022-01-26 12:19:48 +0000

    Add project files.

---
+++ SignalRChatDemo/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+    }
+}
